ad59d2609bd19a5421fe954e48d4d905d6d20a2a
Роман Владиславович Серебряков <rvs@ge74.ru>
2025-08-25 15:39:50 +0500

    create_TelegramController

---
+++ app/Console/Commands/AskMileage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AskMileage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:ask-mileage';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
